db3624d711135ae3d84aa83376700c893831b411
BingNa Yang <bingnayang@gmail.com>
2021-01-27 18:44:35 -0500

    create food-drink path and add path to nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FoodAndDrinkComponent } from './food-and-drink/food-and-drink.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'food-drink', component: FoodAndDrinkComponent},
   {path: '', redirectTo: 'home', pathMatch:'full'}
 ];
 
