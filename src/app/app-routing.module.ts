4b2ebd05384eebdbb00955b15d4f116a8a0f9fb3
BingNa Yang <bingnayang@gmail.com>
2021-01-29 19:53:36 -0500

    create showtime path and add path to nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FoodAndDrinkComponent } from './food-and-drink/food-and-drink.component';
 import { HomeComponent } from './home/home.component';
+import { ShowtimeComponent } from './showtime/showtime.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'food-drink', component: FoodAndDrinkComponent},
+  {path: 'showtime', component: ShowtimeComponent},
   {path: '', redirectTo: 'home', pathMatch:'full'}
 ];
 
