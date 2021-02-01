1cef1f0d5da84b68668a87a8350871bb31bb4751
BingNa Yang <bingnayang@gmail.com>
2021-01-31 20:50:15 -0500

    retrieve data from movie service

--- src/app/showtime/showtime.component.ts
+++ src/app/showtime/showtime.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from '../class/movie';
+import { ShowtimeService} from '../showtime.service';
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-showtime',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./showtime.component.css']
 })
 export class ShowtimeComponent implements OnInit {
+  movieShowList: Movie[];
 
-  constructor() { }
+  constructor(private movieService: MovieService) { }
 
   ngOnInit(): void {
+    this.getSelectedDateShowTimeList();
   }
 
+  private getSelectedDateShowTimeList(){
+    this.movieService.getPlayingMovieList().subscribe(data => {
+      this.movieShowList = data;
+      console.log(this.movieShowList);
+    }, error => console.log(error));
+  }
 }
