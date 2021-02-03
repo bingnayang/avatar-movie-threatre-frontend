adfe7bce07a85d8edd703b4e1b710379412170f5
BingNa Yang <bingnayang@gmail.com>
2021-02-02 19:17:31 -0500

    create showtime-by-movie component

---
+++ src/app/showtime-by-movie/showtime-by-movie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-showtime-by-movie',
+  templateUrl: './showtime-by-movie.component.html',
+  styleUrls: ['./showtime-by-movie.component.css']
+})
+export class ShowtimeByMovieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
