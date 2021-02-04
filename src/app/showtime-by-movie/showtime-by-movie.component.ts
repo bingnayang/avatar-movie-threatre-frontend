2ff98f0c40fef547eefdaaa34c70ba35d0ce7b84
BingNa Yang <bingnayang@gmail.com>
2021-02-04 15:00:35 -0500

    retrieve movie detail by id

--- src/app/showtime-by-movie/showtime-by-movie.component.ts
+++ src/app/showtime-by-movie/showtime-by-movie.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Movie } from '../class/movie';
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-showtime-by-movie',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./showtime-by-movie.component.css']
 })
 export class ShowtimeByMovieComponent implements OnInit {
+  movieShowTime: Movie = new Movie;
+  id: number;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private movieService: MovieService,) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+
+    this.movieService.getMovieById(this.id).subscribe(data => {
+      this.movieShowTime = data;
+      console.log(this.movieShowTime);
+    },error => console.log(error));
   }
 
 }
