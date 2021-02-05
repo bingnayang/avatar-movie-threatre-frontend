0da2eafff51b0bbe186b0f8b5dd4544a26590573
BingNa Yang <bingnayang@gmail.com>
2021-02-05 18:03:23 -0500

    create auditorium-one-imax component

---
+++ src/app/auditorium-one-imax/auditorium-one-imax.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auditorium-one-imax',
+  templateUrl: './auditorium-one-imax.component.html',
+  styleUrls: ['./auditorium-one-imax.component.css']
+})
+export class AuditoriumOneIMAXComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
