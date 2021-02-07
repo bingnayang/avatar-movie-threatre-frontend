1ae8fdfb279abba2134ae45fbc6d66e6a593ea79
BingNa Yang <bingnayang@gmail.com>
2021-02-07 11:18:04 -0500

    create and set auditorium seat. get seat number

--- src/app/auditorium-one-imax/auditorium-one-imax.component.ts
+++ src/app/auditorium-one-imax/auditorium-one-imax.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-auditorium-one-imax',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auditorium-one-imax.component.css']
 })
 export class AuditoriumOneIMAXComponent implements OnInit {
+  id: number;
+  seat: any = ["A1","A2","A3","A4","A5","B1","B2","B3","B4","B5","B6","B7","C1","C2","C3","C4","C5","C6","C7"];
+  seatSelected = [];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    
   }
 
+  selectSeat(seatNumber: string){
+    this.seatSelected.push(seatNumber);
+    console.log("Seat Select: "+seatNumber);
+    console.log(this.seatSelected)
+  }
 }
