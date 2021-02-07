1313d63528e39ebb3430ee5d69f6476a31683681
BingNa Yang <bingnayang@gmail.com>
2021-02-07 11:48:32 -0500

    add seat to array when click, remove if seat already selected

--- src/app/auditorium-one-imax/auditorium-one-imax.component.ts
+++ src/app/auditorium-one-imax/auditorium-one-imax.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auditorium-one-imax',
@@ -11,16 +12,25 @@ export class AuditoriumOneIMAXComponent implements OnInit {
   seat: any = ["A1","A2","A3","A4","A5","B1","B2","B3","B4","B5","B6","B7","C1","C2","C3","C4","C5","C6","C7"];
   seatSelected = [];
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,  private router: Router) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
-    
+
   }
 
   selectSeat(seatNumber: string){
-    this.seatSelected.push(seatNumber);
-    console.log("Seat Select: "+seatNumber);
+    if(this.seatSelected.indexOf(seatNumber) > -1){
+      console.log("Seat already selected")
+      this.seatSelected = this.seatSelected.filter(obj => obj!== seatNumber);
+    }else{
+      this.seatSelected.push(seatNumber);
+      console.log("Seat Select: "+seatNumber);
+    }
     console.log(this.seatSelected)
+  }
+
+  selectTicketType(id: number){
+    this.router.navigate(['ticket-type',id]);
   }
 }
