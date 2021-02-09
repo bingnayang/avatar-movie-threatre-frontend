25ed8289f9e2aee4515208f4fbdfc4b535ab8692
BingNa Yang <bingnayang@gmail.com>
2021-02-09 18:39:46 -0500

    add ticket-type path and auditorium-one-imax path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuditoriumOneIMAXComponent } from './auditorium-one-imax/auditorium-one-imax.component';
 import { FoodAndDrinkComponent } from './food-and-drink/food-and-drink.component';
 import { HomeComponent } from './home/home.component';
 import { MovieInfoComponent } from './movie-info/movie-info.component';
 import { SeeAMovieComponent } from './see-a-movie/see-a-movie.component';
 import { ShowtimeByMovieComponent } from './showtime-by-movie/showtime-by-movie.component';
 import { ShowtimeComponent } from './showtime/showtime.component';
+import { TicketTypeComponent } from './ticket-type/ticket-type.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
@@ -14,6 +16,8 @@ const routes: Routes = [
   {path: 'see-a-movie', component: SeeAMovieComponent},
   {path: 'movie-info/:id', component: MovieInfoComponent},
   {path: 'showtime-by-movie/:id', component: ShowtimeByMovieComponent},
+  {path: 'ticket-type/:id', component: TicketTypeComponent},
+  {path: 'auditorium-one-imax/:id/:starttime_id', component: AuditoriumOneIMAXComponent},
   {path: '', redirectTo: 'home', pathMatch:'full'}
 ];
 
