8b180badd76f974e7a83a2f1b2e1fd7907e5a60d
BingNa Yang <bingnayang@gmail.com>
2021-02-11 12:38:22 -0500

    update seat with available

--- src/app/auditorium-one-imax/auditorium-one-imax.component.ts
+++ src/app/auditorium-one-imax/auditorium-one-imax.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
+import { Movie } from '../class/movie';
+import { MovieService } from '../movie.service';
+import { Seat } from '../class/seat';
 
 @Component({
   selector: 'app-auditorium-one-imax',
@@ -9,22 +12,60 @@ import { Router } from '@angular/router';
 })
 export class AuditoriumOneIMAXComponent implements OnInit {
   id: number;
-  seat: any = ["A1","A2","A3","A4","A5","B1","B2","B3","B4","B5","B6","B7","C1","C2","C3","C4","C5","C6","C7"];
+  starttime_id: number;
+  seat : Seat[] = [
+    {seatNumber:'A1',seatAvailable:'available'},
+    {seatNumber:'A2',seatAvailable:'available'},
+    {seatNumber:'A3',seatAvailable:'available'},
+    {seatNumber:'A4',seatAvailable:'available'},
+    {seatNumber:'A5',seatAvailable:'available'},
+    {seatNumber:'B1',seatAvailable:'available'},
+    {seatNumber:'B2',seatAvailable:'available'},
+    {seatNumber:'B3',seatAvailable:'available'},
+    {seatNumber:'B4',seatAvailable:'available'},
+    {seatNumber:'B5',seatAvailable:'available'},
+    {seatNumber:'B6',seatAvailable:'available'},
+    {seatNumber:'B7',seatAvailable:'available'},
+    {seatNumber:'C1',seatAvailable:'available'},
+    {seatNumber:'C2',seatAvailable:'available'},
+    {seatNumber:'C3',seatAvailable:'available'},
+    {seatNumber:'C4',seatAvailable:'available'},
+    {seatNumber:'C5',seatAvailable:'available'},
+    {seatNumber:'C6',seatAvailable:'available'},
+    {seatNumber:'C7',seatAvailable:'available'},
+  ];
+  
   seatSelected = [];
+  movieInfo: Movie = new Movie;
 
-  constructor(private route: ActivatedRoute,  private router: Router) { }
+  constructor(private route: ActivatedRoute,  private router: Router,private movieService: MovieService) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
+    this.starttime_id = this.route.snapshot.params['starttime_id'];
 
+    this.movieService.getMovieById(this.id).subscribe(data => {
+      this.movieInfo = data;
+      console.log(this.movieInfo);
+    },error => console.log(error));
   }
 
   selectSeat(seatNumber: string){
     if(this.seatSelected.indexOf(seatNumber) > -1){
       console.log("Seat already selected")
       this.seatSelected = this.seatSelected.filter(obj => obj!== seatNumber);
+      this.seat.forEach(function(obj){
+        if(obj.seatNumber === seatNumber){
+          obj.seatAvailable = 'available';
+        }
+      })
     }else{
       this.seatSelected.push(seatNumber);
+      this.seat.forEach(function(obj){
+        if(obj.seatNumber === seatNumber){
+          obj.seatAvailable = 'Hold';
+        }
+      })
       console.log("Seat Select: "+seatNumber);
     }
     console.log(this.seatSelected)
