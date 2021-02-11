2d8333f8f4847d7c2340575d5846ecf4560ff23f
BingNa Yang <bingnayang@gmail.com>
2021-02-11 12:34:33 -0500

    get movie showtime start time and navigate to auditorium page

--- src/app/showtime-by-movie/showtime-by-movie.component.ts
+++ src/app/showtime-by-movie/showtime-by-movie.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Movie } from '../class/movie';
 import { MovieService } from '../movie.service';
+import { Showtime } from '../class/showtime';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-showtime-by-movie',
@@ -11,11 +13,13 @@ import { MovieService } from '../movie.service';
 export class ShowtimeByMovieComponent implements OnInit {
   movieShowTime: Movie = new Movie;
   id: number;
+  start_id: number;
 
-  constructor(private route: ActivatedRoute, private movieService: MovieService,) { }
+  constructor(private route: ActivatedRoute, private movieService: MovieService, private router: Router) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
+    this.start_id = this.route.snapshot.params['start_id'];
 
     this.movieService.getMovieById(this.id).subscribe(data => {
       this.movieShowTime = data;
@@ -23,4 +27,14 @@ export class ShowtimeByMovieComponent implements OnInit {
     },error => console.log(error));
   }
 
+  selectTime(id: number, showtime: Showtime){
+    console.log("Movie Id: ")
+    console.log(id)
+    console.log("ShowTime: ")
+    console.log(showtime)
+
+    if(showtime.auditorium == 'IMAX-3D'){
+      this.router.navigate(['auditorium-one-imax',id,showtime.id]);
+    }
+  }
 }
