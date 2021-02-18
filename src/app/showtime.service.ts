be27628f97d9f4fe85c411cc5882d818b04ceab6
BingNa Yang <bingnayang@gmail.com>
2021-02-18 16:01:25 -0500

    connecting to getShowtime REST API

--- src/app/showtime.service.ts
+++ src/app/showtime.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Showtime } from './class/showtime';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShowtimeService {
+  private baseURL = "http://localhost:8080/api/showtime";
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getShowtime(id: number): Observable<Showtime>{
+    return this.httpClient.get<Showtime>(`${this.baseURL}/${id}`)
+  }
 }
