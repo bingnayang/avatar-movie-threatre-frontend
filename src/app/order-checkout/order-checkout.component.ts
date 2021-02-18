d7aae9508f37502887d1eda16bea260a0ec1c952
BingNa Yang <bingnayang@gmail.com>
2021-02-18 10:38:23 -0500

    get movie info

--- src/app/order-checkout/order-checkout.component.ts
+++ src/app/order-checkout/order-checkout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../movie.service';
+import { Movie } from '../class/movie';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-order-checkout',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-checkout.component.css']
 })
 export class OrderCheckoutComponent implements OnInit {
+  id: number;
+  movieInfo: Movie = new Movie;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private movieService: MovieService) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    // Get movie info
+    this.movieService.getMovieById(this.id).subscribe(data => {
+      this.movieInfo = data;
+      console.log(this.movieInfo);
+    },error => console.log(error));
   }
 
 }
