30d1b12521d271c0098bf3151e48e6dbfa7875f2
BingNa Yang <bingnayang@gmail.com>
2021-02-25 18:11:32 -0500

    create standard auditorium seat

--- src/app/auditorium-standard/auditorium-standard.component.ts
+++ src/app/auditorium-standard/auditorium-standard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Seat } from '../class/seat';
 
 @Component({
   selector: 'app-auditorium-standard',
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auditorium-standard.component.css']
 })
 export class AuditoriumStandardComponent implements OnInit {
+
+  seat : Seat[] = [
+    {seatNumber:'A1',seatAvailable:'available'},
+    {seatNumber:'A2',seatAvailable:'available'},
+    {seatNumber:'A3',seatAvailable:'available'},
+    {seatNumber:'A4',seatAvailable:'available'},
+    {seatNumber:'A5',seatAvailable:'available'},
+    {seatNumber:'B1',seatAvailable:'available'},
+    {seatNumber:'B2',seatAvailable:'available'},
+    {seatNumber:'B3',seatAvailable:'available'},
+    {seatNumber:'B4',seatAvailable:'available'},
+    {seatNumber:'B5',seatAvailable:'available'},
+    {seatNumber:'B6',seatAvailable:'available'}
+  ];
 
   constructor() { }
 
