28ece900e323af57e658d435d246e023ee1995b2
BingNa Yang <bingnayang@gmail.com>
2021-02-26 18:36:59 -0500

    retrieve movie data

--- src/app/auditorium-standard/auditorium-standard.component.ts
+++ src/app/auditorium-standard/auditorium-standard.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Seat } from '../class/seat';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
+import { MovieService } from '../movie.service';
+import { Movie } from '../class/movie';
 
 @Component({
   selector: 'app-auditorium-standard',
@@ -11,6 +13,7 @@ import { Router } from '@angular/router';
 export class AuditoriumStandardComponent implements OnInit {
   id: number;
   starttime_id: number;
+  movieInfo: Movie = new Movie;
   seat : Seat[] = [
     {seatNumber:'A1',seatAvailable:'available'},
     {seatNumber:'A2',seatAvailable:'available'},
@@ -25,11 +28,17 @@ export class AuditoriumStandardComponent implements OnInit {
     {seatNumber:'B6',seatAvailable:'available'}
   ];
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(private route: ActivatedRoute, private router: Router, private movieService: MovieService) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
     this.starttime_id = this.route.snapshot.params['starttime_id'];
+    // Get movie info
+    this.movieService.getMovieById(this.id).subscribe(data => {
+      this.movieInfo = data;
+      console.log(this.movieInfo);
+    },error => console.log(error));
+
   }
 
 }
